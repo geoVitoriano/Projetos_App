32c2b72864dbd00bfbd9a4576f663c2c81d9ed53
geoVitoriano <geofoxit@gmail.com>
2019-09-23 11:59:30 -0300

    Update backend

--- BaseConhecimento/versao-inicial/backend/index.js
+++ BaseConhecimento/versao-inicial/backend/index.js
@@ -1,11 +1,15 @@
 const app = require('express')()
 const consign = require('consign')
+const db = require('./config/db')
+
+app.db = db // importou o knex já configurado
 
 consign()
      // Definição do que será lido 
     .then('./config/middleware.js')
     .then('./api')
     .then('./config/routes.js')
+    .then('')
     .into(app)
 
 app.listen(3000, () => {
