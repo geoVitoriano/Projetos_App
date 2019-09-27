ed032c3e6347ab71b323014d0b26b6254679fbab
geoVitoriano <geofoxit@gmail.com>
2019-09-27 14:51:14 -0300

    Atualização api usuário

--- BaseConhecimento/versao-inicial/backend/config/routes.js
+++ BaseConhecimento/versao-inicial/backend/config/routes.js
@@ -1,6 +1,6 @@
 module.exports = app => {
     app.route('/users')
-       .get(app.api.user.save) // Caminho pelo consign
+       .post(app.api.user.save) // Caminho pelo consign
        .get(app.api.user.get)
 
     app.route('/users/:id') // A partir desse paramentro vai se saber se o sistema está inserindo ou alterando
